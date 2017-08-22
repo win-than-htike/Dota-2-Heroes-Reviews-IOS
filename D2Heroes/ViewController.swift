d949c7f2692c05d3b22a6051690bb428848b2ba8
winnthannhtike <winnthannhtike@gmail.com>
2017-08-22 15:28:30 +0630

    Initial Commit

---
+++ D2Heroes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  D2Heroes
+//
+//  Created by Win Than Htike on 8/22/17.
+//  Copyright © 2017 Win Than Htike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
